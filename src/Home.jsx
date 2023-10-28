b88ef409b3ca0542d8be84b240fb296ee2b537c5
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-10-28 17:46:08 +0600

    Styled-Components installing done

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Shared/Footer";
+import Navbar from "./components/Shared/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      {/* Navber */}
+      <Navbar />
+      {/* Login Container */}
+      <div>
+        <h2>Login Pulse</h2>
+      </div>
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
